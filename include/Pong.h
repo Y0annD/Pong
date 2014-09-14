da58b8c8293cb2cafd3ad910bb49f2a3ec83107b
breizhers <contact@yoann-diquelou.fr>
2014-09-15 00:03:43 +0200

    Pong added

---
+++ include/Pong.h
@@ -0,0 +1,15 @@
+#ifndef PONG
+#define PONG
+
+class Pong{
+ private:
+  std::vector<Mobile*> _mobiles;
+  std::vector<Mur*>     _walls;
+
+  void execute(){}
+  void Collision(){}
+
+
+};
+
+#endif
