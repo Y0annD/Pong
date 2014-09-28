13f746def074e5ec32d1bcec97d75804fa3b2f16
breizhers <contact@yoann-diquelou.fr>
2014-09-28 21:36:56 +0200

    Collision mieux

---
+++ include/Forme.h
@@ -0,0 +1,48 @@
+#ifndef SHAPE
+#define SHAPE
+#include "SFML/Graphics.hpp"
+#include "Color.h"
+#include <iostream>
+#include <string>
+
+class Forme{
+ public:
+  /**
+   * Forme
+   * x: position en x du centre de la forme
+   * y: position en y du centre de la forme
+   * width: largeur de la forme
+   * height: hauteur de la forme
+   * orientation: direction dans laquelle va se déplacer la forme en deg
+   * color: couleur de la forme
+   **/
+ Forme(int x, int y, int width, int height, double orientation, Color color):_x(x), _y(y),
+    _width(width),_height(height), _orientation(orientation), _color(color){}
+
+  void draw(sf::RenderWindow *win) const;
+  
+  // accesseurs
+  inline int getX(){return _x;}
+  inline int getY(){return _y;}
+  inline int getWidth(){return _width;}
+  inline int getHeight(){return _height;}
+  inline double getOrientation(){return _orientation;}
+
+  //mutateurs
+  inline void setX(int x){_x = x;}
+  inline void setY(int y){_y = y;}
+  inline void setWidth(int width){_width = width;}
+  inline void setHeight(int height){_height = height;}
+  inline void setOrientation(int orientation){_orientation = orientation;}
+
+ protected:
+  // shape position
+  double _x, _y;
+  // shape size
+  int _width, _height;
+  //orientation
+  double _orientation;
+  Color _color;
+};
+
+#endif
---
+++ src/Mobile.cpp
@@ -0,0 +1,56 @@
+#include "Mobile.h"
+#include <math.h>
+#define PI 3.1415
+
+/**
+ * Change le sens de la balle en fonction de la collision
+ * @param side: 1 - collision droite
+ * @param side: 2 - collision gauche
+ * @param side: 3 - collision haut
+ * @param side: 4 - collision bas
+ **/
+void Mobile::switchSide(int side ){
+  if(side == 1|| side == 2){
+    _orientation = 180 - _orientation;
+    if(_orientation < 0 )
+      _orientation +=360;
+  }else if(side == 3||side == 4){
+    _orientation = 360 - _orientation;
+    if(_orientation <=0)
+      _orientation +=360;
+  }
+}
+
+
+/** 
+* Méthode permettant de bouger le mobile
+**/
+
+void Mobile::move(){
+  std::cout<<"Orientation: "<<_orientation<<std::endl;
+  double moveX = _speed * cos(PI*_orientation/180.0);
+  double moveY = _speed * sin(PI*_orientation/180.0);
+  /*  int collision = _game.collision(this, getX()+moveX, getY()+moveY);
+  if(collision!=0){
+    if(collision==1){
+      _orientation = 180 - orientation;
+    }else if(collision==2){
+      _orientation += 270;
+      if(_orientation >=360){
+        _orientation -= 360; 
+      }
+      }*/
+  _x += moveX;
+  _y += moveY;
+  std::cout<<"moveX: "<<moveX<<" moveY: "<<moveY<<std::endl;
+}
+
+void Mobile::updateSpeed(int update){
+  _speed += update;
+  if(_speed==0 && update>0)
+    _speed++;
+  if(_speed==0 && update<0)
+    _speed++;
+  if(_speed>5)
+    _speed=5;
+}
